5747fb5412dc7540cab9af3e21ad52b0e02a5d1a
Dino Kunovac <dinokunovac@gmail.com>
2019-12-29 00:41:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly_hash = {railroads: {pieces: 4}, {rent_in_dollars: => }}
+  monopoly_hash = {railroads: {pieces: 4}, {rent_in_dollars:}}
   
 end
 
